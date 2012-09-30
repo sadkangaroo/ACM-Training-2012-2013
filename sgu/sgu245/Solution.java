bc339af37e1f58430f0c6b112be611f26efb13df
sadkangaroo <liushuang931006@gmail.com>
2012-09-30 15:15:11 +0800

    unsolved

---
+++ sgu/sgu245/Solution.java
@@ -0,0 +1,10 @@
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        Scanner cin = new Scanner(System.in); 
+        int N = cin.nextInt(), M = cin.nextInt();
+    }
+}
